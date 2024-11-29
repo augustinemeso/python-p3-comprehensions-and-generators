d49908d684c8dbf72139c8a386fed43fdda1462f
augustinemeso <augustinemesobk@gmail.com>
2024-11-29 17:25:44 +0300

    add updated PYTHON-P3-COMPREHENSIONS-AND-GENERATORS

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
-#!/usr/bin/env python3
+# list_comprehension.py
 
-def return_evens(num_list):
-    pass
+def return_evens(seq):
+    return [x for x in seq if x % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+def make_exclamation(sentences):
+    return [sentence + "!" for sentence in sentences]
+
+# Add test calls under the main condition
+if __name__ == "__main__":
+    print(return_evens([0, 1, 3, 5, 7, 8, 9]))  # Should print: [0, 8]
+    print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))  # Should print: ["Hello!", "I'm doing great!", "Python is fun!"]
